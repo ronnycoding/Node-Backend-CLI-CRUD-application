dd9264c4d5fb3a97509ea1a898f04caba6b48a54
Ronny <ronnyangelo.freites@gmail.com>
2018-08-04 14:49:36 -0600

    Add Readme

---
+++ README.md
@@ -0,0 +1,22 @@
+# Node Backend Application CRUD CLI
+
+### Installation
+
+Requires [Node.js](https://nodejs.org/) v10
+
+Install the dependencies and devDependencies and start the server.
+
+```sh
+$ cd Node-Backend-CLI-CRUD-application
+$ npm install -d
+```
+
+Command list
+
+```sh
+$ node app --help
+$ node app create --help
+$ node app update --help
+$ node app delete --help
+$ node app list --help
+```
--- config/yargs.js
+++ config/yargs.js
@@ -1,46 +1,49 @@
+const description = {
+    demand: false,
+    alias: 'd',
+    desc: 'Task description'
+}
+
+const id = {
+    demand: true,
+    alias: 'i',
+    desc: 'Task id'
+}
+
+const completed = {
+    demand: false,
+    alias: 'c',
+    default: true,
+    desc: 'Mark as completed an existing task'
+}
+
+const init = {
+    demand: false,
+    alias: 'i',
+    default: 1
+}
+
+const end = {
+    demand: false,
+    alias: 'end',
+    default: null
+}
+
 const yargs = require('yargs')
     .command('create', 'Create a new task', {
-        description: {
-            demand: true,
-            alias: 'd',
-            'desc': 'Task description'
-        }
+        description
     })
     .command('update', 'Update existing task', {
-        id: {
-            demand: true,
-            alias: 'i',
-            'desc': 'Task id'
-        },
-        description: {
-            demand: false,
-            alias: 'd',
-            'desc': 'Task description'
-        },
-        completed: {
-            demand: false,
-            alias: 'c',
-            default: true,
-            desc: 'Mark as completed an existing task'
-        }
-    })
-    .command('list', 'Tasks list', {
-        init: {
-            demand: false,
-            alias: 'i',
-            default: 1
-        },
-        end: {
-            demand: false,
-            alias: 'end',
-            default: null
-        }
+        id,
+        description,
+        completed
     })
     .command('delete', 'Delete a Task', {
-        id: {
-            demand: true,
-            alias: 'i'
-        }
+        id
+    })
+    .command('list', 'Tasks list', {
+        init,
+        end
     })
     .help()
     .argv;
